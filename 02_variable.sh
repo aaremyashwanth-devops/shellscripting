7e7ff00b22f79a84d596f97062367a454f6b8fc5
Yashwanth Aarem <aaremyashwanth@gmail.com>
2026-01-31 21:55:59 +0530

    code is not adding

---
+++ 02_variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1 :: hello "
+echo "$PERSON2 :: fine "
+echo "enter another name"
+read USER_NAME
+echo "i m new call me $USER_NAME"
+echo "everything ok"

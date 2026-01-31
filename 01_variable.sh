d0881ec623026964ce5bd7ba198b7fd474d78bf3
Yashwanth Aarem <aaremyashwanth@gmail.com>
2026-01-31 21:37:38 +0530

    error coming

--- 01_variable.sh
+++ 01_variable.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
-PERSON1=$1
-PERSON2=$2
+# PERSON1=$1
+# PERSON2=$2
 PERSON3="superstar"
-echo "$PERSON ::HELLO"
-echo "$PERSON :: I am fine"
-echo "i m the $PERSON3"
+echo "$PERSON3 ::HELLO"
+echo "$PERSON3 :: I am fine"
+echo "i m the $PERSON3" 
+echo "error is coming"

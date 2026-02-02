eb257354e24d29ff0d3ae3f04de5c0e4aa602b06
Yashwanth Aarem <aaremyashwanth@gmail.com>
2026-02-02 12:24:15 +0530

    timesleep

---
+++ 04_timestart.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "how much time taken for excution"
+START_TIME=$(date)
+echo "time started $START_TIME"
+sleep 10
+END_TIME=$(date)
+TOTAL_TIME=$((START_TIME-END_TIME))
+echo "final time consumed $TOTAL_TIME"

10d590d77559140d1c976c79354c2b9ea80099a5
Yashwanth Aarem <aaremyashwanth@gmail.com>
2026-02-02 12:29:46 +0530

    %s

--- 04_timestart.sh
+++ 04_timestart.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "how much time taken for excution"
-START_TIME=$(date)
+START_TIME=$(date+%s)
 echo "time started $START_TIME"
 sleep 10
-END_TIME=$(date)
+END_TIME=$(date+%s)
 TOTAL_TIME=$(($START_TIME-$END_TIME))
 echo "final time consumed $TOTAL_TIME"

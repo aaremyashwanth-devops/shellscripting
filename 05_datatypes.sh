96b550175d725702ce17f4bf722c72a5637d4472
Yashwanth Aarem <aaremyashwanth@gmail.com>
2026-02-02 12:52:50 +0530

    2datatypes multiple

--- 05_datatypes.sh
+++ 05_datatypes.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 NAME="Yashwanth"
 AGE=26
+AGE1=3
 echo "my name is $NAME"
 echo "my age is $AGE"
+
+#array list
+ITEMS=("LIGHT","BULB","KEYBOARD","PEN")
+echo "accessing first item $ITEMS[0]"
+echo "accessing secound item ${ITEMS[1]}"
+#addition
+echo "$(($AGE + 2)):: result"
+echo "addition two ages are $((AGE+AGE1))

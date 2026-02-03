824ae785b35119d026ea5aac93fd92d1141ad2d4
Yashwanth Aarem <aaremyashwanth@gmail.com>
2026-02-03 13:08:10 +0530

    function

---
+++ 08_function.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+USER_ID=$(id -u)
+VALIDATE(){
+    if [ USER_ID -eq 0 ]; then
+     echo "you are root user"
+     echo "Ready to install dnf $1"
+     exit 1
+    else 
+     echo "please switch to root user"
+    fi
+}
+VALIDATE "installing $package"

7cf85c601b4d35f56377f7e9381fdeecd28a1e67
Yashwanth Aarem <aaremyashwanth@gmail.com>
2026-02-03 13:32:38 +0530

    lops

--- 08_function.sh
+++ 08_function.sh
@@ -5,11 +5,10 @@ VALIDATE(){
     if [ $USER_ID -eq 0 ]; then
      echo "you are root user"
      echo "Ready to install dnf $1"
-     dnf install $2
-     echo "entered package name $2"
+     
      exit 1
     else 
      echo "please switch to root user"
     fi
 }
-VALIDATE "installing mysql" $2
+VALIDATE "installing mysql"

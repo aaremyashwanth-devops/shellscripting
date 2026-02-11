ed4123e44d4c1ead9c734904359ed209c8ad249a
aaremyashwanth <111639396+Aaremyashwanth@users.noreply.github.com>
2026-02-11 15:27:01 +0530

    Delete 06_conditions.sh

--- 06_conditions.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-NUMBER=$1
-if [ $NUMBER -gt 20 ]; then
- echo "this is true $NUMBER"
-elif [ $NUMBER -eq 20 ]; then
- echo "this equal :number $NUMBER"
-else 
-  echo "this is false $NUMBER"
-fi

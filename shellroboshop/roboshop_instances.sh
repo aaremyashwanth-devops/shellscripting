de15a75faf486a2762bd00cf5ba543d91f8183a7
Yashwanth Aarem <aaremyashwanth@gmail.com>
2026-02-12 01:45:10 +0530

    corrected variable name

--- shellroboshop/roboshop_instances.sh
+++ shellroboshop/roboshop_instances.sh
@@ -6,17 +6,15 @@ ZONE_ID="Z017691837EJOVFVM6G3X"
 
 for instance in $@
  do 
- INSTANCE_ID=$(aws ec2 run-instances \
- --image-id $ami_id \
- --instance-type t3.micro \
- --security-group-ids $sg_id\ 
- --associate-public-ip-address \
- --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
- --query 'Instances[0].InstanceId' \
- --output text)
-
-
-
+    INSTANCE_ID=$(aws ec2 run-instances \
+    --image-id $iam_id \
+    --instance-type t3.micro \
+    --security-group-ids $sg_id \ 
+    --associate-public-ip-address \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query 'Instances[0].InstanceId' \
+    --output text
+ )
 
     if [ "frontend" == $instance ]; then
         IP=$(aws ec2 describe-instances \

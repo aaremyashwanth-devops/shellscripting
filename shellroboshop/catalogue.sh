9df003108751774147be32bb29dc9cbaa5a9ac06
Yashwanth Aarem <aaremyashwanth@gmail.com>
2026-02-12 15:59:24 +0530

    catalogue setup

---
+++ shellroboshop/catalogue.sh
@@ -0,0 +1,73 @@
+#1/bin/bash
+USER_ID=$(id -u)
+LOGFOLDER="/var/log/shelllog"
+LOGFILE="$LOGFOLDER/$0.log"
+CURRENT_DIR=$PWD
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\E[0m"
+if [ $USER_ID -ne 0 ]; then
+    echo  "switch to root user"
+    exit1
+else
+    echo "you are root user"
+fi
+mkdir -p $LOGFOLDER
+validate(){
+    if [ $? -ne 0 ]; then
+        echo -e "$2...$R failure$N" | tee -a $LOGFILE
+    else
+        echo -e "$2...$R successfull$N" |tee -a $LOGFILE
+}
+
+dnf module disable nodejs -y  &>>$LOGFILE
+validate $? "module disable"  
+
+dnf module enable nodejs:20 -y &>>LOGFILE
+validate $? "enable nodejs"
+
+dnf install nodejs -y &>>LOGFILE
+validate $? "install nodejs"
+
+id roboshop &>>LOGFILE
+if [ $? -ne 0 ]; then
+    useradd  --system --home /app --shell /sbin/nologin --comment"create user roboshop" roboshop &>>LOGFILE
+    validate $? "useradd"
+else 
+    echo -e " skipping"
+fi
+
+mkdir /app &>>$LOGFILE
+validate $? "app dictory created"
+
+cd /app &>>LOGFILE
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOGFILE
+unzip /tmp/catalogue.zip
+
+npm install &>>LOGFILE
+
+cp $CURRENT_DIR/catalogue.service /etc/systemd/system/catalogue.service 
+validate $? "service file is created"
+
+systemctl daemon-reload
+
+systemctl enable catalogue
+systemctl start catalogue 
+validate $? "catalogue started"
+
+cp $CURRENT_DIR/mongo.repo /etc/yum.repo.d/mongo.repo
+dnf install mongodb-org -y 
+
+INDEX=$(mongosh --host $MONGODB_HOST --quiet  --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+
+if [ $INDEX -le 0 ]; then
+    mongosh --host $MONGODB_HOST </app/db/master-data.js
+    VALIDATE $? "Loading products"
+else
+    echo -e "Products already loaded ... $Y SKIPPING $N"
+fi
+
+systemctl restart catalogue
+VALIDATE $? "Restarting catalogue"

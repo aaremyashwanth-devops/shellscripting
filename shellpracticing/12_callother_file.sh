cb9c9b83cfbc9db92e297ae4da30977e5c7896ef
Yashwanth Aarem <aaremyashwanth@gmail.com>
2026-02-13 12:13:36 +0530

    calling the another shellscript

---
+++ shellpracticing/12_callother_file.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+COUNTRY=india
+echo "this is from script one $COUNTRY"
+sh 13_file2.sh
+
+echo "PID OF script one $$"
---
+++ shellpracticing/13_file2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+COUNTRY=USA
+echo "this is from second script $COUNTRY"
+echo "PID OF SCRIPT 2..$$"

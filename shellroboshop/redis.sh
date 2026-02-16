d5c47139284cd638eebde6430e2b093608b7711c
Yashwanth Aarem <aaremyashwanth@gmail.com>
2026-02-16 14:19:34 +0530

    replace code

--- shellroboshop/redis.sh
+++ shellroboshop/redis.sh
@@ -1,20 +1,39 @@
 #!/bin/bash
-USER_ID=$(id -u)
-LOGFOLDER="/var/log/shelllog"
-LOGFILE="$LOGFOLDER/$0.log"
 
-if [ $USER_ID -ne 0 ]; then
-    echo "you are  not root user"
+USERID=$(id -u)
+LOGFOLDER="/var/log/shelllog"
+LOGFILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGFILE
     exit 1
-else
-    echo "you are root user"
 fi
-mkdir -p /shelllog
+
+mkdir -p $LOGFOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGFILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGFILE
+    fi
+}
+
 dnf module disable redis -y &>>$LOGFILE
-dnf module enable redis:7 -y
-dnf install redis -y
+dnf module enable redis:7 -y &>>$LOGFILE
+VALIDATE $? "Enable Redis:7"
+
+dnf install redis -y  &>>$LOGFILE
+VALIDATE $? "Installed Redis"
 
 sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+VALIDATE $? "Allowing remote connections"
 
-systemctl enable redi
-systemctl start redis
+systemctl enable redis &>>$LOGFILE
+systemctl start redis 
+VALIDATE $? "Enabled and started Redis"
